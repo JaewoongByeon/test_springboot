ab0fc969e466a9ccf20d269d04dd164d91caa270
JaewoongByeon <jwbyeon330@gmail.com>
2019-08-14 16:28:41 +0900

    hello

--- src/main/java/com/example/test_springboot/controller/HelloController.java
+++ src/main/java/com/example/test_springboot/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping(value = "/resthello")
     public String hello() {
-        return "Hello World!";
+        return "Welcome to Hello World Application!";
     }
 }
